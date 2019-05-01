bebd1c7ed04bbbfc3ff7ffb86d4242335a1e46b0
Beth Secor <bsecor@telligen.com>
2019-05-01 21:07:00 +0000

    Simplify folder structure

--- data.py
+++ data.py
@@ -1,4 +1,4 @@
 from narratives.coded.parse import parse_atlas_output
 
-parse_atlas_output('/home/bsecor/ncc-narratives/data/docx/C.2 Q16 S+C ALL CREATED CODES.docx')
+parse_atlas_output('./data/C.2 Q16 S+C ALL CREATED CODES.docx')
 
