8322eff7cf3aec65aa5982398163b024492aa7c5
Beth Secor <bsecor@telligen.com>
2019-05-17 18:06:40 +0000

    Add variable for train and test

--- data.py
+++ data.py
@@ -2,6 +2,9 @@ from narratives.coded.parse import parse_atlas_output
 from narratives.coded.convert import convert_doc2docx
 from glob import glob
 from pandas import pandas
+import numpy
+from numpy.random import choice
+from collections import Counter
 import feather
 
 frames = []
@@ -10,4 +13,11 @@ for doc in glob('./data/*.doc'):
     frames.append(parse_atlas_output(doc + "x"))
 
 data = pandas.concat(frames)
-feather.write_dataframe(data, './data/coded_data.feather')
+uniq_segment = numpy.unique(data['segment'])
+data_uniq = pandas.DataFrame({'uniq_segment': uniq_segment, 
+                              'dataset': choice(['TRAIN','TEST'],len(uniq_segment),p=(0.7, 0.3))})
+merge_data = data.merge(data_uniq, how='left', left_on='segment', right_on='uniq_segment')
+final_data = merge_data.drop(columns=['uniq_segment'])
+print(Counter(final_data['dataset']))
+
+feather.write_dataframe(final_data, './data/coded_data.feather')
