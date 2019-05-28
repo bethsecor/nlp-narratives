bb71c58a9747034a8501078490b9d92cb2206be4
Beth Secor <bsecor@telligen.com>
2019-05-28 20:47:46 +0000

    Debug process of removing stop words

--- narratives/coded/process.py
+++ narratives/coded/process.py
@@ -1,16 +1,20 @@
 from nltk.corpus import stopwords
-from nltk.tokenize import word_tokenize
+from nltk.tokenize import sent_tokenize, word_tokenize
 stopWords = set(stopwords.words('english'))
 
-def remove_stopwords(string):
-    words = word_tokenize(string)
-    wordsFiltered = []
+def remove_stopwords(stringList):
+    removedStopWords = []
+    for string in stringList:
+        words = word_tokenize(string)
+        wordsFiltered = []
+        
+        for w in words:
+            if w not in stopWords:
+                wordsFiltered.append(w)
 
-    for w in words:
-        if w not in stopWords:
-            wordsFiltered.append(w)
+        removedStopWords.append(' '.join(wordsFiltered))
+    return removedStopWords
 
-    return ' '.join(wordsFiltered)
 
 def process(df, segment):
     df[segment + "_swr"] = remove_stopwords(df[segment])
