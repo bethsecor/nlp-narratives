2fdf3f3cde7073c1b8eada12a875af83f6209111
Beth Secor <bsecor@telligen.com>
2019-05-29 19:45:22 +0000

    Try porter stemming

--- narratives/coded/process.py
+++ narratives/coded/process.py
@@ -1,21 +1,28 @@
 from nltk.corpus import stopwords
 from nltk.tokenize import sent_tokenize, word_tokenize
+from nltk.stem import PorterStemmer
+porter=PorterStemmer()
 stopWords = set(stopwords.words('english'))
 
-def remove_stopwords(stringList):
-    removedStopWords = []
+def process_segments(stringList, function):
+    processedList = []
     for string in stringList:
         words = word_tokenize(string)
-        wordsFiltered = []
-        
-        for w in words:
-            if w not in stopWords:
-                wordsFiltered.append(w)
+        wordsProcessed = []
 
-        removedStopWords.append(' '.join(wordsFiltered))
-    return removedStopWords
+        for w in words:
+            if function == "stem":
+                wordsProcessed.append(porter.stem(w))
+            elif function == "stopwords":
+                if w not in stopWords:
+                    wordsProcessed.append(w)
+
+        processedList.append(' '.join(wordsProcessed))
+    return processedList
 
 
 def process(df, segment):
-    df[segment + "_swr"] = remove_stopwords(df[segment])
+    df[segment + "_swr"] = process_segments(df[segment], "stopwords")
+    df[segment + "_swr_stem"] = process_segments(df[segment + "_swr"], "stem")
+
     return df
