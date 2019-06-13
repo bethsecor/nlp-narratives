f58075d8de5581e33ae6887911b3790a461e1b9e
Beth Secor <bsecor@telligen.com>
2019-06-13 21:37:49 +0000

    Save model evaluation metrics to CSV

--- fit.py
+++ fit.py
@@ -3,7 +3,9 @@ from collections import Counter
 from narratives.models.logistic import logistic
 from narratives.models.naivebayes import naivebayes
 from narratives.models.supportvector import supportvector
+from sklearn.metrics import cohen_kappa_score, accuracy_score
 import joblib
+import pandas
 
 data = feather.read_dataframe('./data/coded_data.feather')
 
@@ -19,11 +21,24 @@ data = data[x_vars + y_vars_10 + ['dataset']]
 train = data[data.dataset == 'TRAIN']
 test = data[data.dataset == 'TEST']
 
-for code in y_vars_10:
+model_evaluation = pandas.DataFrame({'code':[],'x':[],'method':[],'dataset':[],'accuracy':[],'cohen_kappa':[]})
+for code in ['Communication']:
 #    for x in x_vars:
-    lr = logistic(train, code, 'segment')
+    x = 'segment'
+    lr = logistic(train, code, x)
     joblib.dump(lr, './models/' + code.replace(' ', '\ ').replace('/', '-') + '.joblib')
+    lr_pred_train = lr.predict(train[x])
+    model_evaluation = model_evaluation.append(pandas.DataFrame({'code':[code],'x':[x],'method':['logistic'],'dataset':['train'],
+                            'accuracy':[accuracy_score(train[code], lr_pred_train)],
+                            'cohen_kappa':[cohen_kappa_score(train[code], lr_pred_train)]}))
+    lr_pred_test = lr.predict(test[x])
+    model_evaluation = model_evaluation.append(pandas.DataFrame({'code':[code],'x':[x],'method':['logistic'],'dataset':['test'],
+                            'accuracy':[accuracy_score(test[code], lr_pred_test)],
+                            'cohen_kappa':[cohen_kappa_score(test[code], lr_pred_test)]}))
 #       nb = naivebayes(train, code, x)
 #       print(nb)
 #       svm = supportvector(train, code, x)
 #       print(svm)
+
+print(model_evaluation)
+model_evaluation.to_csv('./models/model_evaluation.csv')
